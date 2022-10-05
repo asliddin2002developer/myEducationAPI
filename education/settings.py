6ab82a6033547f9e89e59987dfa05449669e202a
Asliddin <eshonkulovasliddindeveloper@gmail.com>
2022-10-05 17:53:30 +0500

    second commit

--- education/settings.py
+++ education/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,8 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6cc(g$-(hgi(+$hune1=(mlm8=^f#-1wsl_a-#j=w0sy_a(%34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
@@ -105,7 +105,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
